4e7f39e3b738726c8762c003d0305ff2198de3a6
Cristian Nazareno Gonzalez <contact@cristianngonzalez.com>
2022-02-21 10:28:55 -0300

    adapt to screen

--- scripts/musicplay.js
+++ scripts/musicplay.js
@@ -1,3 +1,17 @@
+//Va en true si la musica se esta reproduciendo
+let audio = document.getElementById("audio")
+let btnAudio = document.getElementById("btnAudio");
+let musicplay = false;
+
 document.getElementById("btnAudio").addEventListener("click",function(){
-  document.getElementById("audio").play();
+  if(musicplay){
+    audio.pause();
+    btnAudio.innerHTML = '<i class="bi bi-volume-mute-fill"></i>';
+    musicplay = false;
+  }else{
+    //Play music
+    audio.play();
+    btnAudio.innerHTML = '<i class="bi bi-volume-up-fill"></i>';
+    musicplay = true;
+  }
 });
